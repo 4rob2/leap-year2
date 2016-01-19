289b630e58f294783ca19dc5c4c18cebf1add88d
blair peterson and rob reasoner <blairchalmers@gmail.com>
2016-01-19 14:19:31 -0800

    update specs

--- specs/specs.js
+++ specs/specs.js
@@ -1,4 +1,18 @@
 describe("leapYear", function() {
-  it("is false for the year that is not divisible by 4, 100, or 400", function() {expect(leapYear(2001)).to.equal(false);
+  it("is false for a year that is not divisible by 4, 100, or 400", function()
+  {
+    expect(leapYear(2001)).to.equal(false);
+  });
+
+  it("is divisible by 4", function() {
+    expect(leapYear(2004)).to.equal(true);
+  });
+
+  it("is false for years divisible by 100", function() {
+    expect(leapYear(2300)).to.equal(false);
+  });
+
+  it("is true for years divisible by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
   });
 });
